1041a279937a892358a5296696a5452c5bb7ec1f
S-zebra <shimashimazebra13@gmail.com>
2018-09-19 01:02:09 +0900

    投稿のピン表示機能仮実装

--- tukumokkumokku/MapViewController.swift
+++ tukumokkumokku/MapViewController.swift
@@ -15,6 +15,8 @@ class MapViewController: UIViewController {
   var locationManager: CLLocationManager!
   var currentLocation: CLLocationCoordinate2D?
   let zoomedSpan = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
+  var annotations: [MKPointAnnotation] = [MKPointAnnotation]()
+  let api = TsukumoAPI.shared
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -58,6 +60,28 @@ extension MapViewController: CLLocationManagerDelegate {
   func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
     currentLocation = locations[0].coordinate
     mapView.setCenter(currentLocation!, animated: true)
+    // 投稿を取得
+    api.getPosts(location: currentLocation!, onComplete: { posts in
+      posts.forEach({ post in
+        let locationOfPost = CLLocationCoordinate2DMake(CLLocationDegrees(post.lat), CLLocationDegrees(post.lon))
+
+        //同一のものは飛ばす
+        self.annotations.forEach({ a in
+          if a.coordinate.latitude == locationOfPost.latitude
+            && a.coordinate.longitude == locationOfPost.longitude {
+            return
+          }
+        })
+
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locationOfPost
+        annotation.title = post.text.prefix(7).appending("…") // 7文字ぐらい？
+        self.annotations.append(annotation)
+        DispatchQueue.main.async {
+          self.mapView.addAnnotations(self.annotations)
+        }
+      })
+    })
   }
 
   func showAlert(title: String, message: String) {
