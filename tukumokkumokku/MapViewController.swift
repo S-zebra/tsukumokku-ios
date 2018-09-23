862eb1736b5dd011777056202203c3eaebf863ac
S-zebra <shimashimazebra13@gmail.com>
2018-09-23 18:58:35 +0900

    過去のnotifierを削除するようにした マップの現在地追尾処理の追加 (なぜか丸が出ない…)

--- tukumokkumokku/MapViewController.swift
+++ tukumokkumokku/MapViewController.swift
@@ -12,13 +12,15 @@ import UIKit
 
 class MapViewController: UIViewController {
   @IBOutlet var mapView: MKMapView!
+  @IBOutlet var panGestureRcg: UIPanGestureRecognizer!
+  @IBOutlet var currentLocationButton: UIButton!
+
   var locationManager: CLLocationManager!
   var currentLocation: CLLocationCoordinate2D?
+  var notifiers = [UILocalNotification]()
   let zoomedSpan = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
   let annotationTitleCount = 7
   let api = TsukumoAPI.shared
-
-  var corrected = false
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -26,6 +28,9 @@ class MapViewController: UIViewController {
     locationManager.delegate = self
     locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
     mapView.delegate = self
+    mapView.showsUserLocation = true
+    panGestureRcg.delegate = self
+    panGestureRcg.addTarget(self, action: #selector(onMapPanned(_:)))
     // Do any additional setup after loading the view.
   }
 
@@ -33,18 +38,35 @@ class MapViewController: UIViewController {
     locationManager.startUpdatingLocation()
     let newRegion = MKCoordinateRegionMake(mapView.region.center, zoomedSpan)
     mapView.setRegion(newRegion, animated: true)
+    updatePosts()
+  }
+
+  @objc func onMapPanned(_ sender: Any) {
+    currentLocationButton.isSelected = false
+  }
+
+  @IBAction func currentLocationButtonTapped(_ sender: Any) {
+    currentLocationButton.isSelected = true
+    if currentLocation != nil {
+      mapView.setRegion(MKCoordinateRegionMake(currentLocation!,
+                                               mapView.region.span),
+                        animated: true)
+    }
   }
 
   @IBAction func refreshButtonTappeed(_ sender: Any) {
-    corrected = false
     updatePosts()
   }
 
   func updatePosts() {
     // 投稿を取得
-    api.getPosts(location: currentLocation!, onComplete: { posts in
+    api.getPosts(location: mapView.region.center, onComplete: { posts in
+      DispatchQueue.main.async {
+        self.notifiers.forEach({ item in
+          UIApplication.shared.cancelLocalNotification(item)
+        })
+      }
       var annotations = [MKAnnotation]()
-      var notifiers = [UILocalNotification]()
       posts.forEach({ post in
         let annotation = MKPointAnnotation()
         annotation.coordinate = CLLocationCoordinate2DMake(CLLocationDegrees(post.lat), CLLocationDegrees(post.lon))
@@ -54,12 +76,13 @@ class MapViewController: UIViewController {
           annotation.title = post.text // 7文字ぐらい？
         }
         annotations.append(annotation)
-        notifiers.append(self.createNotifier(post: post, radius: 30))
+        self.notifiers.append(self.createNotifier(post: post, radius: 30))
       })
       DispatchQueue.main.async {
         self.mapView.removeAnnotations(self.mapView.annotations)
         self.mapView.addAnnotations(annotations)
-        notifiers.forEach({ item in
+        self.notifiers.forEach({ item in
+          NSLog("Notification regisitered, \(item.region?.description)")
           UIApplication.shared.scheduleLocalNotification(item)
         })
       }
@@ -78,7 +101,10 @@ class MapViewController: UIViewController {
     notification.alertBody = "この付近に投稿があります" // メッセージ
     notification.region = region // 領域
     notification.regionTriggersOnce = false // 一度のみか否か
+    NSLog("\(notification.description)")
     notification.category = AppDelegate.nearPostsNotificationCat
+
+    locationManager.startMonitoring(for: region)
     return notification
   }
 
@@ -97,6 +123,12 @@ extension MapViewController: MKMapViewDelegate {
   }
 }
 
+extension MapViewController: UIGestureRecognizerDelegate {
+  func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+    return true
+  }
+}
+
 extension MapViewController: CLLocationManagerDelegate {
   func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
     switch status {
@@ -112,11 +144,9 @@ extension MapViewController: CLLocationManagerDelegate {
 
   func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
     currentLocation = locations[0].coordinate
-    NSLog("Location Updated. Lat: \(currentLocation?.latitude), Lon: \(currentLocation?.longitude )")
-    if !corrected {
-      mapView.setRegion(MKCoordinateRegionMake(currentLocation!, zoomedSpan), animated: true)
-      corrected = true
-      updatePosts()
+    NSLog("Location Updated. Lat: \(currentLocation?.latitude), Lon: \(currentLocation?.longitude)")
+    if currentLocationButton.isSelected {
+      mapView.setRegion(MKCoordinateRegionMake(currentLocation!, mapView.region.span), animated: true)
     }
   }
 }
